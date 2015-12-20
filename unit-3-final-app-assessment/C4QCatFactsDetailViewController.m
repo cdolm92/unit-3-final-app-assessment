37df83efe2d2951df9c33add57347025b3c0daf2
cdolm92 <cdolm92@gmail.com>
2015-12-20 00:39:07 -0500

    giphy api works

--- unit-3-final-app-assessment/C4QCatFactsDetailViewController.m
+++ unit-3-final-app-assessment/C4QCatFactsDetailViewController.m
@@ -7,10 +7,14 @@
 //
 
 #import "C4QCatFactsDetailViewController.h"
+#import <AFNetworking/AFNetworking.h>
 
 #define CAT_GIF_URL @"http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
 
 @interface C4QCatFactsDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *giphyImage;
+@property (nonatomic) NSMutableArray *imageUrls;
 
 @end
 
@@ -18,7 +22,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+ 
+    
+    NSSet *contentTypes = [NSSet setWithObjects: @"text/html", @"text/plain", @"audio/wav", @"application/javascript", @"application/json", nil];
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer.acceptableContentTypes = contentTypes;
+    
+    [manager GET:@"http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
+      parameters:nil
+       progress:nil
+        success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+            
+             NSArray *data = [responseObject objectForKey:@"data"];
+            
+            self.imageUrls = [[NSMutableArray alloc]init];
+             
+            for(NSDictionary * images in data) {
+                
+                NSDictionary *image = [images objectForKey:@"images"];
+                
+                NSDictionary *catImage = [image objectForKey:@"fixed_height_still"];
+                
+                NSString *catURLImage = [catImage objectForKey:@"url"];
+                
+                [self.imageUrls addObject: catURLImage];
+
+                
+               
+                
+               
+                
+            }
+
+         
+            
+            
+             int random = arc4random_uniform((int)self.imageUrls.count);
+              NSString *ImageURL = [self.imageUrls objectAtIndex:random];
+              NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:ImageURL]];
+              self.giphyImage.image = [UIImage imageWithData:imageData];
+            
+                NSLog(@"%@", ImageURL);
+             }
+     
+         failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+             
+             
+             NSLog(@"%@", error.userInfo);
+         }];
+    
+
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
